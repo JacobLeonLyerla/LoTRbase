f61a48dec49a5dfe6189a25ff429c74d049b615a
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-09 21:35:44 -0700

    app not working today

--- lotrbase/src/App.js
+++ lotrbase/src/App.js
@@ -1,17 +1,14 @@
-import {useEffect,useState,} from "React"
+import {useEffect,useState,} from "react"
 import './App.css';
 import axios from "axios"
 function App() {
   const[books,setBooks] = useState([])
   const lordAPI =()=>{
   axios
-  .get(`https://the-one-api.dev/v2/book?api_key${
+  .get(`https://the-one-api.dev/v2/book?api_key=${
     process.env.REACT_APP_LOTR_API_KEY}`)
   .then(response => {
-    this.setState({
-      movies: response.data.results,
-      totalPages: response.data.total_pages
-    });
+  console.log(response)
   })
   .catch(err => console.log(err));
   }
