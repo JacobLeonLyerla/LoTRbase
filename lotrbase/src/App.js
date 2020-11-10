3d51a173ccd67fd9c60335b1c24486ad5ecf6fbf
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-09 21:21:36 -0700

    setting up api call

--- lotrbase/src/App.js
+++ lotrbase/src/App.js
@@ -1,10 +1,22 @@
-
+import {useEffect,useState,setState} from "React"
 import './App.css';
-
+import axios from "axios"
 function App() {
+  const lordAPI =()=>{
+  axios
+  .get(`https://the-one-api.dev/v2/book?api_key${
+    process.env.REACT_APP_LOTR_API_KEY}`)
+  .then(response => {
+    this.setState({
+      movies: response.data.results,
+      totalPages: response.data.total_pages
+    });
+  })
+  .catch(err => console.log(err));
+  }
   return (
     <div className="App">
-      
+      a
     </div>
   );
 }
