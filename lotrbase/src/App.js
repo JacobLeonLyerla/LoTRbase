4839bf45c56062a18ba3fb3ba489744173b8a11e
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-10 22:37:21 -0700

    removed code

--- lotrbase/src/App.js
+++ lotrbase/src/App.js
@@ -4,13 +4,7 @@ import axios from "axios"
 function App() {
   const[books,setBooks] = useState([])
   const lordAPI =()=>{
-  axios
-  .get(`https://the-one-api.dev/v2/book?api_key=${
-    process.env.REACT_APP_LOTR_API_KEY}`)
-  .then(response => {
-  console.log(response)
-  })
-  .catch(err => console.log(err));
+ 
   }
   useEffect(() => {
    const api = lordAPI()
