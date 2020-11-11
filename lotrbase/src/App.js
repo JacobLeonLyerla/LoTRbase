c1f548c1dfc3464ee1327fe21b3d268c8e7fc4e4
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-10 22:49:41 -0700

    api may not be working I need to run it in postman and see if it even works

--- lotrbase/src/App.js
+++ lotrbase/src/App.js
@@ -4,13 +4,22 @@ import axios from "axios"
 function App() {
   const[books,setBooks] = useState([])
   const lordAPI =()=>{
- 
+    console.log('api',process.env)
+  axios
+  .get(`https://the-one-api.dev/v2/book?api_key=${
+    process.env.REACT_APP_LOTR_API_KEY}`)
+  .then(response => {
+  console.log(response.data,"here")
+  })
+  .catch(err => console.log(err));
   }
   useEffect(() => {
    const api = lordAPI()
+   console.log('api',process.env.REACT_APP_LOTR_API_KEY)
    setBooks(api)
     
   });
+  console.log(books)
   return (
     <div className="App">
       a
