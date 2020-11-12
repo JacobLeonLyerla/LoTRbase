f4ff6af6c1817ebe1203cff76cdd41e72c7404df
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-11 21:00:36 -0700

    formatting

--- lotrbase/src/App.js
+++ lotrbase/src/App.js
@@ -1,33 +1,27 @@
-import {useEffect,useState,} from "react"
-import './App.css';
-import axios from "axios"
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
-  const[books,setBooks] = useState([])
-  const lordAPI =()=>{
+  const [books, setBooks] = useState([]);
+  const lordAPI = () => {
     const config = {
-      headers: { Authorization: `Bearer ${process.env.REACT_APP_LOTR_API_KEY}` }
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_LOTR_API_KEY}`,
+      },
+    };
+    axios
+      .get("https://the-one-api.dev/v2/./movie", config)
+      .then((response) => {
+        console.log(response, "here");
+      })
+      .catch((err) => console.log(err));
   };
-  axios
-  .get(
-    'https://the-one-api.dev/v2/./movie',
-    config
-  )
-  .then(response => {
-  console.log(response,"here")
-  })
-  .catch(err => console.log(err));
-  }
   useEffect(() => {
-   const api = lordAPI()
-   setBooks(api)
-    
+    const api = lordAPI();
+    setBooks(api);
   });
-  console.log(books)
-  return (
-    <div className="App">
-      a
-    </div>
-  );
+  console.log(books);
+  return <div className="App">a</div>;
 }
 
 export default App;
