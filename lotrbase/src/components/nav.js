fbf754feddfd2058fb8b987e00bd3ea4ad1ea495
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-17 23:29:15 -0700

    links should now work, can't test because my api calls are exhausted

--- lotrbase/src/components/nav.js
+++ lotrbase/src/components/nav.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 
 const Navigation = (props) => {
-
+console.log(props)
 
   return (
     <div>
       <Nav pills>
         <NavItem>
-          <NavLink href="#">Movies</NavLink>
+          <NavLink href="#" onClick={()=>props.lordAPI("movie")}>Movies</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="#">Books</NavLink>
+          <NavLink href="#" onClick={()=>props.lordAPI("book")}>Books</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink disabled href="#">Characters</NavLink>
+          <NavLink disabled href="#" onClick={()=>props.lordAPI("character")}>Characters</NavLink>
         </NavItem>
       </Nav>
     </div>
