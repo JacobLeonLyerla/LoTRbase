240e482b162d6423350c4c5a93e2df0e796f4c14
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-17 23:18:08 -0700

    set up cards

--- lotrbase/src/components/lotrCard.js
+++ lotrbase/src/components/lotrCard.js
@@ -7,16 +7,13 @@ import {LOTRContext} from '../contexts/lotrProvider';
 
 const LOTRCard = () => {
   const {books, setBooks} = useContext(LOTRContext)
+  console.log(books)
   return (<CardDeck>
     {books.map(book => {
       return  (
         <Card>
-          <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
           <CardBody>
             <CardTitle tag="h5">{book.name}</CardTitle>
-            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-            <Button>Button</Button>
           </CardBody>
         </Card>
       
