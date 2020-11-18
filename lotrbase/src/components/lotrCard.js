e5743f042db45f18aa556ea8a7d6bab5c17f0884
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-17 23:11:17 -0700

    rending cards with titles

--- lotrbase/src/components/lotrCard.js
+++ lotrbase/src/components/lotrCard.js
@@ -7,41 +7,24 @@ import {LOTRContext} from '../contexts/lotrProvider';
 
 const LOTRCard = () => {
   const {books, setBooks} = useContext(LOTRContext)
-  return (<div>
+  return (<CardDeck>
     {books.map(book => {
-      return  <div>{book.name}</div>
+      return  (
+        <Card>
+          <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+          <CardBody>
+            <CardTitle tag="h5">{book.name}</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+      
+      )
      }
    
-   )}</div>
-    // <CardDeck>
-    //   <Card>
-    //     <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-    //     <CardBody>
-    //       <CardTitle tag="h5">Card title</CardTitle>
-    //       <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-    //       <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-    //       <Button>Button</Button>
-    //     </CardBody>
-    //   </Card>
-    //   <Card>
-    //     <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-    //     <CardBody>
-    //       <CardTitle tag="h5">Card title</CardTitle>
-    //       <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-    //       <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-    //       <Button>Button</Button>
-    //     </CardBody>
-    //   </Card>
-    //   <Card>
-    //     <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-    //     <CardBody>
-    //       <CardTitle tag="h5">Card title</CardTitle>
-    //       <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-    //       <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-    //       <Button>Button</Button>
-    //     </CardBody>
-    //   </Card>
-    // </CardDeck>
+   )}</CardDeck>
+   
   );
 };
 
